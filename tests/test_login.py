e7aaa71bf50d05ac471fcf8f105422d48ed39e8b
Andrei Trofin <trofinandrei1@gmail.com>
2026-05-17 22:57:52 +0300

    update class TestLogin

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,14 +8,14 @@ class TestLogin:
     def test_valid_login(self, logged_in_page):
         assert logged_in_page.url.endswith("/inventory.html")
 
-    def test_invalid_login(self, page):
-        login_page = LoginPage(page)
+    def test_invalid_login(self, navigated_page):
+        login_page = LoginPage(navigated_page)
         login_page.login("wrong_user", "wrong_password")
         error = login_page.get_error_message()
         assert "Username and password do not match" in error
 
-    def test_empty_credentials(self, page):
-        login_page = LoginPage(page)
+    def test_empty_credentials(self, navigated_page):
+        login_page = LoginPage(navigated_page)
         login_page.login("", "")
         error = login_page.get_error_message()
         assert "Username is required" in error
